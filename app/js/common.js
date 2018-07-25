53831057533a96ac91b5e2a88071261b50a0639d
AlexandrovVladimir <dev.alexandrovhub@gmail.com>
2018-07-25 21:13:02 +0300

    fancyBox, tabs+slick

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,4 @@
-(function (){
+(function () {
 
 /*  full page slider */
   $('.gallery').slick({
@@ -92,30 +92,6 @@
   //count and active slides
 
 /*/  news slider */
-
-/*  section studio about */
-  const studioLink = document.querySelector('.studio__link');
-  const studioAbout = document.querySelector('.studio-about');
-  const studioClose = document.querySelector('.studio-about__close');
-
-  studioLink.addEventListener('click', function(e) {
-    e.preventDefault();
-
-    if (studioLink.classList.contains('studio-about-active')) {
-      studioAbout.classList.remove('studio-about-active');
-      document.body.classList.remove('body-locked');
-    } else {
-      studioAbout.classList.add('studio-about-active');
-      document.body.classList.add('body-locked');
-    }
-  });
-  studioClose.addEventListener('click', function(e) {
-    e.preventDefault();
-    studioAbout.classList.remove('studio-about-active');
-    document.body.classList.remove('body-locked');
-  });
-
-/*/  section studio about */
 
 /*  projects slider */
   $('.projects__viewport').slick({
--- app/js/logo.js
+++ app/js/logo.js
@@ -5,10 +5,9 @@ $(document).ready(function () {
         let galleryH = $('.gallery').innerHeight();
         let newsH = $('.news-orange').innerHeight();
         let studioH = $('.studio-yellow').innerHeight();
-        console.log(galleryH);
-        console.log(newsH);
-        console.log(scroll_pos);
+
         scroll_pos = $(this).scrollTop();
+
         if (scroll_pos > (galleryH + newsH - 100)) {
             $('.header__logo_link').css('backgroundColor', '#4eb748');
         } else {
